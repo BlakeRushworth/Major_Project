b41d9c3360da5c64e4ff7da2aeeb84f6562bbbd8
BlakeRushworth <blake@bitbot.com.au>
2025-08-24 19:21:41 +1000

    walls spawn correctly
    however some areas are not accessible

--- Assets/map_generation.cs
+++ Assets/map_generation.cs
@@ -2,45 +2,130 @@ using UnityEngine;
 
 public class map_generation : MonoBehaviour
 {
-    public GameObject prefab;
+    public GameObject GridBlock;
+    //public GameObject testprefab;
+    public GameObject Hwall;
+    public GameObject Vwall;
+
+    public Vector2 grid_coordinate;
 
     private Vector3 spawn_coords;
     private int map_size_x;
     private int map_size_y;
     const int tile_size = 16;
+    const int wall_percentage = 3;
     void Start()
     {
-        map_size_x = Random.Range(3, 6);
-        map_size_y = Random.Range(3, 6);// picks map size from 3x3 to 5x5
-        for ( int x = 0; x < map_size_x * tile_size; x = x+16)
+        Grid_Logic();
+    }
+
+
+    void Update()
+    {
+
+    }
+    public void Hwall_Logic()
+    {
+        for (int x = 1; x < map_size_x * tile_size; x = x + tile_size)
         {
-            for (int y = 0; y < map_size_y * tile_size; y = y+16)
+            for (int y = tile_size - 1; y < (map_size_y - 1) * tile_size; y = y + tile_size)
             {
-                print("( " + x + " , " + y + " )");
                 spawn_coords = new Vector3(x, y, 0);
-                spawn();
+                int chance = Random.Range(0, wall_percentage);
+                if (chance == 0)
+                {
+                    print("Hwall at: ( " + (x + 1) / tile_size + " , " + (y + 1) / tile_size + " )");
+                    print("Hwall touching: (" + (x - 1) / tile_size + " , " + (y + 1) / tile_size + " )");
+                    print("and ( " + (x - 1) / tile_size + " , " + ((y + 1) - tile_size) / tile_size + " )");
+                    Spawn_Hwall();
+                }
+            }
+        }
+    }
+    public void Vwall_Logic()
+    {
+        for (int x = tile_size - 1; x < (map_size_x - 1) * tile_size; x = x + tile_size)
+        {
+            for (int y = 1; y < map_size_y * tile_size; y = y + tile_size)
+            {
+                spawn_coords = new Vector3(x, y, 0);
+                int chance =  Random.Range(0, wall_percentage);
+                if (chance == 0)
+                {
+                    print("Vwall at: ( " + (x + 1) / tile_size + " , " + (y + 1) / tile_size + " )");
+                    print("Vwall touching: (" + ((x + 1) - tile_size) / tile_size + " , " + (y - 1) / tile_size + " )");
+                    print("and ( " + (x + 1) / tile_size + " , " + (y - 1) / tile_size + " )");
+                    Spawn_Vwall();
+                }
+                
             }
         }
     }
 
-    
-    void Update()
+    public void Spawn_Vwall()
     {
-       
-    }
-
-    public void spawn()
-    {
-        if (prefab != null)
+        //print("Vwall");
+        if (Vwall != null)
         {
             // Instantiate the prefab at a specific position and rotation
-            Instantiate(prefab, spawn_coords, Quaternion.identity);
-            Debug.Log("Prefab instantiated successfully!");
+            Instantiate(Vwall, spawn_coords, Quaternion.identity);
+            //Debug.Log("Prefab instantiated successfully!");
         }
         else
         {
-            Debug.LogError("Prefab not found at path: " + prefab);
+            Debug.LogError("Prefab not found at path: " + Vwall);
         }
     }
 
+
+    public void Spawn_Hwall()
+    {
+        //print("Hwall");
+        if (Hwall != null)
+        {
+            // Instantiate the prefab at a specific position and rotation
+            Instantiate(Hwall, spawn_coords, Quaternion.identity);
+            //Debug.Log("Prefab instantiated successfully!");
+        }
+        else
+        {
+            Debug.LogError("Prefab not found at path: " + Hwall);
+        }
+    }
+
+
+
+    public void Grid_Logic()
+    {
+        map_size_x = Random.Range(3, 6);
+        map_size_y = Random.Range(3, 6);// picks map size from 3x3 to 5x5
+        for (int x = 0; x < map_size_x * tile_size; x = x + tile_size)
+        {
+            for (int y = 0; y < map_size_y * tile_size; y = y + tile_size)
+            {
+                grid_coordinate.x = x / tile_size;
+                grid_coordinate.y = y / tile_size;
+                print(" grid: " + grid_coordinate);
+                spawn_coords = new Vector3(x, y, 0);
+                Spawn_Grid();
+            }
+        }
+        
+        Vwall_Logic();
+        Hwall_Logic();
+        
+    }
+    public void Spawn_Grid()
+    {
+        if (GridBlock != null)
+        {
+            // Instantiate the prefab at a specific position and rotation
+            Instantiate(GridBlock, spawn_coords, Quaternion.identity);
+            //Debug.Log("Prefab instantiated successfully!");
+        }
+        else
+        {
+            Debug.LogError("Prefab not found at path: " + GridBlock);
+        }
+    }
 }
